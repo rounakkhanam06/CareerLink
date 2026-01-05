75a6d11d4bc43aac453008906f17a2ab7b2f364a
Rounak khanam <161000523+rounakkhanam06@users.noreply.github.com>
2026-01-05 16:18:38 +0530

    fix env file

--- frontend/src/config/index.js
+++ frontend/src/config/index.js
@@ -1,9 +1,9 @@
+
+
 import axios from 'axios';
 
-
-export const BASE_URL = 'http://localhost:5000';
+export const BASE_URL = process.env.NEXT_PUBLIC_API_URL; 
 
 export const clientServer = axios.create({
     baseURL: BASE_URL,
 });
-
