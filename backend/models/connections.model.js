ae2bb6601cf2a4c103fd2e1cbdd98555fbb3f034
Rounak khanam <161000523+rounakkhanam06@users.noreply.github.com>
2026-02-15 19:32:33 +0530

    Updated connections model and server logic

--- backend/models/connections.model.js
+++ backend/models/connections.model.js
@@ -1,23 +1,3 @@
-// import mongoose from 'mongoose';
-
-// const connectionSchema = new mongoose.Schema({
-//     userId:{
-//         type: mongoose.Schema.Types.ObjectId,
-//         ref: 'User',
-//     },
-//     connectionId:{
-//         type: mongoose.Schema.Types.ObjectId,
-//         ref: 'User',
-//     },
-//     status_accepted:{
-//         type: Boolean,
-//         default: null,
-//     }
-// }); 
-
-// const ConnectionRequest = mongoose.model('ConnectionRequest', connectionSchema);
-// export default ConnectionRequest;
-
 import mongoose from "mongoose";
 
 const connectionSchema = new mongoose.Schema(
